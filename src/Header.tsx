694183c0b78718de0139fc9acee54d79f4865b7f
Miahri <mahrihommadova@gmail.com>
2023-04-28 22:50:50 +0300

    Added content of app's header

---
+++ src/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <img src="#" alt="logo"/>
+            <input type="text"/>
+            <button>Home</button>
+            <button>Stories</button>
+            <button>Videos</button>
+            <button>Group</button>
+            <button>Shop</button>
+            <button>Notifications</button>
+            <button>Chats</button>
+            <button>Settings</button>
+            <button>Profile</button>
+        </div>
+    );
+};
+
+export default Header;
